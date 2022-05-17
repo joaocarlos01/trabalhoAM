c6921316239a32f877e4add07fcb8f7abe09e4b9
joaocarlos01 <picassojoao01@gmail.com>
2022-05-17 22:53:26 +0100

    Mapa e Código v1
    Mapa e Código v1 do labirinto

---
+++ Codigo_Lab v1/gameObject.js
@@ -0,0 +1,126 @@
+// gameObjects.js 
+
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+
+class GameObject extends EventTarget {
+
+    constructor(x, y, width, height) {
+        super();
+
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+
+    update() {
+       
+    }
+
+    draw() {
+     
+
+    }
+}
+
+class Sprite extends GameObject {
+
+    static imagem;
+
+    constructor(x, y, width, height) {
+        super(x, y, width, height);
+    }
+
+    update() {
+        
+    }
+
+    draw() {
+
+
+        ctx.drawImage(this.constructor.imagem, this.x, this.y, this.width, this.height);
+    }
+
+    static load(urlImagem) {
+
+
+        this.imagem = new Image();
+
+        this.imagem.addEventListener("load", () => {
+            window.dispatchEvent(new CustomEvent('assetLoad', { detail: this }))
+        });
+
+        this.imagem.src = urlImagem;
+    }
+}
+
+class AnimatedSprite extends Sprite {
+
+    static numberFrames;
+    static numberFramesPerRow;
+    static slice;
+
+    constructor(x, y, width, height) {
+        super(x, y, width, height);
+
+        this.currentFrame = 1;
+
+        this.sx = 0;
+        this.sy = 0;
+    }
+
+    update() {
+
+
+        this.currentFrame++;
+
+        if (this.currentFrame > this.constructor.numberFrames)
+            this.currentFrame = 1;
+
+        let deltaX = (this.currentFrame - 1) % this.constructor.numberFramesPerRow;
+        let deltaY = Math.floor((this.currentFrame - 1) / this.constructor.numberFramesPerRow);
+
+        this.sx = deltaX * this.constructor.slice.width;
+        this.sy = deltaY * this.constructor.slice.height;
+    }
+
+
+    draw() {
+
+
+        ctx.drawImage(this.constructor.imagem, this.sx, this.sy, this.constructor.slice.width, this.constructor.slice.height,
+            this.x, this.y, this.width, this.height);
+    }
+
+    static load(urlImagem, numberFrames, numberFramesPerRow) {
+
+        this.imagem = new Image();
+
+        this.imagem.src = urlImagem;
+
+        this.imagem.addEventListener('load', () => {
+
+            this.numberFrames = numberFrames;
+            this.numberFramesPerRow = numberFramesPerRow;
+
+            this.slice = {};
+            this.slice.width = this.imagem.width / numberFramesPerRow;
+
+            let numberRows = Math.ceil(numberFrames / numberFramesPerRow);
+            this.slice.height = this.imagem.height / numberRows;
+
+            window.dispatchEvent(new CustomEvent('assetLoad', { detail: this }));
+
+        });
+
+    }
+
+}
+
+
+
+
+
+
+
