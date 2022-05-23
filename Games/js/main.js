a92c55e1be4132be190b11eea4799d4f75792974
F-Esteves <francisco.jp.esteves@outlook.pt>
2022-05-23 11:43:20 +0100

    Solução dos bugs

--- Games/js/main.js
+++ Games/js/main.js
@@ -1,7 +1,8 @@
-let canvas = document.getElementById('canvas');
-let context = canvas.getContext('2d');
-let canvas_parent = document.querySelector('.canvas-parent');
+"use strict";
 
+let canvas = document.getElementById("canvas");
+let context = canvas.getContext("2d");
+let canvas_parent = document.querySelector(".canvas-parent");
 
 var parent_height = canvas_parent.clientHeight;
 var parent_width = canvas_parent.clientWidth;
@@ -11,73 +12,82 @@ canvas.width = parent_width;
 
 context.textAlign = "center";
 context.textBaseline = "middle";
-context.fillText('Click in this area', canvas.height / 2, canvas.width / 2);
+context.fillText("Click in this area", canvas.height / 2, canvas.width / 2);
 
-let start = document.getElementById('start');
-let time_text = document.getElementById('time-text');
+let start = document.getElementById("start");
+let time_text = document.getElementById("time-text");
 
-let GameStatus = {
+const GameStatus = {
     STOP: 1,
     START: 2,
-}
+};
 
-let stats = GameStatus.STOP;
+const backgroundColors = {
+    green: "rgb(78, 197, 78)",
+    red: "rgb(206, 63, 63)",
+    yellow: "rgb(237, 255, 172)",
+};
 
-function get_random_time(min, max){
+let currentStatus = GameStatus.STOP;
+
+let currentTime, countDownTimer;
+
+let userCanClick = false;
+
+canvas.addEventListener("click", updateTimeOnClick);
+
+function get_random_time(min, max) {
     let result = Math.floor(Math.random() * Math.floor(max)) + min;
     result = result * 1000;
     return result;
 }
 
-function end_game(){
-    clearTimeout(timeout1);
-    clearTimeout(timeout2);
-    canvas.style.background = "rgb(237, 255, 172)";
+function end_game() {
+    canvas.style.background = backgroundColors.yellow;
     start.innerHTML = "Start Game";
-    stats = GameStatus.STOP;
+    currentStatus = GameStatus.STOP;
+    userCanClick = false;
 }
 
-function timeout_function1(time){
-    timeout1 = setTimeout(function(){
-    canvas.style.background = "rgb(78, 197, 78)";
-    let date1 = new Date();
-    let time_now = date1.getTime();
+function startTimer(time) {
+    countDownTimer = setTimeout(function () {
+        let date1 = new Date();
+        currentTime = date1.getTime();
+        canvas.style.background = backgroundColors.green;
 
-    canvas.addEventListener('click', function(){
-        let date2 = new Date();
-        time_later = date2.getTime();
-        play_time = (time_later - time_now);
-        time_text.innerHTML = play_time + "ms";
-    });
-}, time);
-}
-
-function timeout_function2(time){
-    timeout2 = setTimeout(function(){
-        end_game();
-
+        userCanClick = true;
     }, time);
-    }
-
-function start_game(){
-    let change_time = get_random_time(1,8);
-    let end_time = change_time + 5000;
-    stats = GameStatus.START;
-    canvas.style.background = "rgb(206, 63, 63)";
-
-    timeout_function1(change_time);
-    timeout_function2(end_time);
 }
 
-start.addEventListener('click', function(){
-    if(stats === GameStatus.START){
+function start_game() {
+    let change_time = get_random_time(1, 8);
+    currentStatus = GameStatus.START;
+    canvas.style.background = backgroundColors.red;
+
+    startTimer(change_time);
+}
+
+start.addEventListener("click", function () {
+    if (currentStatus === GameStatus.START) {
         end_game();
-    }else{
+    } else {
         start_game();
         this.innerHTML = "Stop Game";
     }
 });
 
-canvas.addEventListener('click', function(){
-   end_game();
-});
+function updateTimeOnClick() {
+    if (currentStatus === GameStatus.STOP) return;
+
+    if (userCanClick) {
+        let date2 = new Date();
+        let time_later = date2.getTime();
+        let play_time = time_later - currentTime;
+        time_text.innerHTML = play_time + "ms";
+    } else {
+        time_text.textContent = "Too soon.";
+        clearTimeout(countDownTimer);
+    }
+
+    end_game();
+}
