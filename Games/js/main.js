27d7fc7c126120fd7e633c498f4254d4d1826e59
F-Esteves <francisco.jp.esteves@outlook.pt>
2022-05-23 14:44:30 +0100

    Implementação de BootStrap para mudança de visual

--- Games/js/main.js
+++ Games/js/main.js
@@ -1,21 +1,15 @@
-"use strict";
+var tempo = document.getElementById("tempo");
+
+tempo.style.opacity = "0.5";
 
 let canvas = document.getElementById("canvas");
 let context = canvas.getContext("2d");
-let canvas_parent = document.querySelector(".canvas-parent");
 
-var parent_height = canvas_parent.clientHeight;
-var parent_width = canvas_parent.clientWidth;
-
-canvas.height = parent_height;
-canvas.width = parent_width;
-
-context.textAlign = "center";
-context.textBaseline = "middle";
-context.fillText("Click in this area", canvas.height / 2, canvas.width / 2);
-
-let start = document.getElementById("start");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+let start = document.getElementById("tempo");
 let time_text = document.getElementById("time-text");
+
 
 const GameStatus = {
     STOP: 1,
@@ -25,16 +19,13 @@ const GameStatus = {
 const backgroundColors = {
     green: "rgb(78, 197, 78)",
     red: "rgb(206, 63, 63)",
-    yellow: "rgb(237, 255, 172)",
+    blue: "rgb(115, 147, 179)",
 };
+canvas.style.background = backgroundColors.blue;
 
 let currentStatus = GameStatus.STOP;
 
 let currentTime, countDownTimer;
-
-let userCanClick = false;
-
-canvas.addEventListener("click", updateTimeOnClick);
 
 function get_random_time(min, max) {
     let result = Math.floor(Math.random() * Math.floor(max)) + min;
@@ -43,51 +34,44 @@ function get_random_time(min, max) {
 }
 
 function end_game() {
-    canvas.style.background = backgroundColors.yellow;
-    start.innerHTML = "Start Game";
+
+    let date2 = new Date();
+    let time_later = date2.getTime();
+    let play_time = time_later - currentTime;
+    tempo.innerHTML = play_time + "ms";
+    canvas.style.background = backgroundColors.blue;
+
     currentStatus = GameStatus.STOP;
-    userCanClick = false;
+
 }
 
 function startTimer(time) {
-    countDownTimer = setTimeout(function () {
+
+    countDownTimer = setTimeout(function() {
+        tempo.style.pointerEvents = "auto";
+
         let date1 = new Date();
         currentTime = date1.getTime();
         canvas.style.background = backgroundColors.green;
-
-        userCanClick = true;
+        tempo.innerHTML = "CLICK!!"
     }, time);
 }
 
 function start_game() {
-    let change_time = get_random_time(1, 8);
+    tempo.style.pointerEvents = "none";
+
+    let change_time = get_random_time(1, 3);
     currentStatus = GameStatus.START;
     canvas.style.background = backgroundColors.red;
-
+    tempo.innerHTML = "...";
     startTimer(change_time);
 }
 
-start.addEventListener("click", function () {
+tempo.addEventListener("click", function() {
+
     if (currentStatus === GameStatus.START) {
         end_game();
     } else {
         start_game();
-        this.innerHTML = "Stop Game";
     }
 });
-
-function updateTimeOnClick() {
-    if (currentStatus === GameStatus.STOP) return;
-
-    if (userCanClick) {
-        let date2 = new Date();
-        let time_later = date2.getTime();
-        let play_time = time_later - currentTime;
-        time_text.innerHTML = play_time + "ms";
-    } else {
-        time_text.textContent = "Too soon.";
-        clearTimeout(countDownTimer);
-    }
-
-    end_game();
-}
